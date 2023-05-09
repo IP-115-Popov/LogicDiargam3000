4b2306dc1e984f36af2eae3b977c43f4c5ee9156
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-09 20:28:07 +0700

    Добавьте файлы проекта.

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicDiagram3000.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
