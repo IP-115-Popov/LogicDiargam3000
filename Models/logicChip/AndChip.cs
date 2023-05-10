7cff5b5afdf1298b0746b0a315a93f33dc8b3e29
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 21:01:57 +0700

    Add skeleton project

---
+++ Models/logicChip/AndChip.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDiagram3000.Models.logicChip
+{
+    public class AndChip : ChipToIn
+    {
+    }
+}
