6603205a695f74a2b7f1ff9f11c50b46496e8702
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 20:48:55 +0700

    Chip View

---
+++ Views/logicScheme/OrChipView.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace LogicDiagram3000.Views.logicScheme
+{
+    public class OrChipView : TemplatedControl
+    {
+    }
+}
