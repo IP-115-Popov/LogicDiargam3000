56514286dfae628c813a6fccddd3f60119111c4b
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 21:13:26 +0700

    Add StringToThicknessConverter

---
+++ Models/Converters/StringToThicknessConverter.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDiagram3000.Models.Converters
+{
+    public class StringToThicknessConverter : IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return Avalonia.Thickness.Parse(((Point)value).ToString());
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return ((Thickness)value).ToString();
+        }
+    }
+}
