2464d991d2073ec45cfaea4f1821a525c20ec952
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 21:21:14 +0700

    Fix Connector

--- Models/Connectors/Connector.cs
+++ Models/Connectors/Connector.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Avalonia;
+using DynamicData.Binding;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,33 @@ using System.Threading.Tasks;
 
 namespace LogicDiagram3000.Models.Connectors
 {
-    public class Connector
+    public class Connector : AbstractNotifyPropertyChanged
     {
+        private Point startPoint;
+        private Point endPoint;
+        public Point StartPoint
+        {
+            get => startPoint;
+            set
+            {
+                SetAndRaise(ref startPoint, value);
+            }
+        }
+        public Point EndPoint
+        {
+            get => endPoint;
+            set
+            {
+                SetAndRaise(ref endPoint, value);
+            }
+        }
+        public void ChangeStartPoint(Point dPoint)
+        {
+            this.StartPoint += dPoint;
+        }
+        public void ChangeEndPoint(Point dPoint)
+        {
+            this.EndPoint += dPoint;
+        }
     }
 }
