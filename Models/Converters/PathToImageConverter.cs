c6548b58b698592d2bd340a06b543370ff67109b
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 22:01:31 +0700

    Add PathToImageConverter

---
+++ Models/Converters/PathToImageConverter.cs
@@ -0,0 +1,47 @@
+﻿using Avalonia;
+using Avalonia.Data.Converters;
+using Avalonia.Platform;
+using System;
+using System.Globalization;
+using System.Reflection;
+using Avalonia.Media.Imaging;
+
+namespace LogicDiagram3000.Models.Converters
+{
+    public class PathToImageConverter : IValueConverter
+    {
+        public static PathToImageConverter Instance = new PathToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string rawUri && targetType.IsAssignableFrom(typeof(Bitmap)))
+            {
+                Uri uri;
+
+                // Allow for assembly overrides
+                if (rawUri.StartsWith("avares://"))
+                {
+                    uri = new Uri(rawUri);
+                }
+                else
+                {
+                    string assemblyName = Assembly.GetEntryAssembly().GetName().Name;
+                    uri = new Uri($"avares://{assemblyName}/{rawUri}");
+                }
+
+                var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+                var asset = assets.Open(uri);
+
+                return new Bitmap(asset);
+            }
+            return null;
+            //throw new NotSupportedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
+}
