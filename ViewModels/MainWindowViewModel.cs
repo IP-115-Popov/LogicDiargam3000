740ec83ce95b2ecd2e8f435fe5f9e1814483d6c4
IP-115-Popov <99480504+IP-115-Popov@users.noreply.github.com>
2023-05-10 23:06:09 +0700

    Selection Chip panel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -9,6 +9,11 @@ namespace LogicDiagram3000.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private bool addChipDraw;
+        private bool orChipDraw;
+        private bool notChipDraw;
+        private bool xorChipDraw;
+        private bool demultiplexerChipDraw;
         private ObservableCollection<object> canvasList;
         public MainWindowViewModel()
         {
@@ -24,6 +29,31 @@ namespace LogicDiagram3000.ViewModels
         {
             get => canvasList;
             set => this.RaiseAndSetIfChanged(ref canvasList, value);
+        }
+        public bool AddChipDraw
+        {
+            get => addChipDraw;
+            set => this.RaiseAndSetIfChanged(ref addChipDraw, value);
+        }
+        public bool OrChipDraw
+        {
+            get => orChipDraw;
+            set => this.RaiseAndSetIfChanged(ref orChipDraw, value);
+        }
+        public bool NotChipDraw
+        {
+            get => notChipDraw;
+            set => this.RaiseAndSetIfChanged(ref notChipDraw, value);
+        }
+        public bool XorChipDraw
+        {
+            get => xorChipDraw;
+            set => this.RaiseAndSetIfChanged(ref xorChipDraw, value);
+        }
+        public bool DemultiplexerChipDraw
+        {
+            get => demultiplexerChipDraw;
+            set => this.RaiseAndSetIfChanged(ref demultiplexerChipDraw, value);
         }
 
     }
